d30ba7c24d1f5096029d5973e1b55af0aca9de68
LukasFoltyn <fol.lukyn@gmail.com>
2021-11-14 19:38:48 +0100

    add .NET project for building API

---
+++ ChessWebAPI/Models/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChessWebAPI.Models
+{
+    public class Game
+    {
+        [Key]
+        public Guid GameId { get; set; }
+        
+        [Required(ErrorMessage = "Duration field is required")]
+        public string Duration { get; set; }
+        
+        [Required(ErrorMessage = "Moves field is required")]
+        public string Moves { get; set; }
+        public DateTime Date { get; set; }
+        public Guid FirstPlayerId { get; set; }
+        public Player FirstPlayer { get; set; }
+        public Guid SecondPlayerId { get; set; }
+        public Player SecondPlayer { get; set; }
+    }
+}
