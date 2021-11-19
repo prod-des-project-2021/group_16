4205fa44342eef6832ea8db9a7ea120a4919ccc9
LukasFoltyn <fol.lukyn@gmail.com>
2021-11-19 15:37:46 +0100

    UnitOfWork pattern was applied for wrapping the repositories

---
+++ ChessWebAPI/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using ChessWeb.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChessWebAPI
+{
+    public interface IUnitOfWork
+    {
+        public IGameRepository Game { get; }
+        public IPlayerRepository Player { get; }
+        void Complete();
+        void Dispose();
+    }
+}
---
+++ ChessWebAPI/UnitOfWork.cs
@@ -0,0 +1,56 @@
+﻿using ChessWeb.DAL;
+using ChessWeb.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChessWebAPI
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly AppDbContext _dbContext;
+        private IGameRepository gameRepository;
+        private IPlayerRepository playerRepository;
+
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IGameRepository Game 
+        {
+            get
+            {
+                if (gameRepository == null)
+                {
+                    gameRepository = new GameRepository(_dbContext);
+                }
+
+                return gameRepository;
+            }
+        }
+
+        public IPlayerRepository Player
+        {
+            get
+            {
+                if (playerRepository == null)
+                {
+                    playerRepository = new PlayerRepository(_dbContext);
+                }
+
+                return playerRepository;
+            }
+        }
+        public void Complete()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+    }
+}
