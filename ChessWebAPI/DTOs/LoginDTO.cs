9f8e7c5b165292816d93ae373c0e41da153043ac
LukasFoltyn <fol.lukyn@gmail.com>
2021-11-19 15:41:18 +0100

    added DTOs for account controller and its mapper profiles

---
+++ ChessWebAPI/DTOs/LoginDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChessWebAPI.DTOs
+{
+    public class LoginDTO
+    {
+        [Required(ErrorMessage = "Username field is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password field is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ ChessWebAPI/MapperProfiles/SignUpProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ChessWeb.DAL.Models;
+using ChessWebAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChessWebAPI.MapperProfiles
+{
+    public class SignUpProfile : Profile
+    {
+        public SignUpProfile()
+        {
+            CreateMap<SignUpDTO, Player>();
+        }
+    }
+}
