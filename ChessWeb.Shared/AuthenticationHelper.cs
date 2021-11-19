76cd5f599f88ff35e4e44674d85c4878c3125962
LukasFoltyn <fol.lukyn@gmail.com>
2021-11-19 15:34:15 +0100

    library project crated for shared resources

---
+++ ChessWeb.Shared/AuthenticationHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace ChessWeb.Shared
+{
+    public static class AuthenticationHelper
+    {
+        public static ClaimsPrincipal CreateClaims(Guid userId, string username, string email)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.Email, email)
+            };
+
+            ClaimsIdentity userIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return new ClaimsPrincipal(userIdentity);
+        }
+    }
+}
