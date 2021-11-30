4dbf250b269bd219051cc35be05485ded6e1658e
LukasFoltyn <fol.lukyn@gmail.com>
2021-11-30 09:14:43 +0100

    Add mapper profiles for easier converting DTOs to database entities and the other way around

---
+++ ChessWebAPI/MapperProfiles/GameProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ChessWeb.DAL.Models;
+using ChessWeb.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChessWebAPI.MapperProfiles
+{
+    public class GameProfiles : Profile
+    {
+        public GameProfiles()
+        {
+            CreateMap<Game, AllGamesDTO>();
+            CreateMap<CreateGameDTO, Game>();
+            CreateMap<Game, DetailGameDTO>();
+        }
+    }
+}
